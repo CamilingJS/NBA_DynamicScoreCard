d389275edff70a4f2aeac059653413977c5044bc
Jonathan <jonathancamiling@gmail.com>
2021-09-27 23:19:52 -0700

    refactored to make function based

--- app.js
+++ app.js
@@ -1,111 +1,128 @@
 const warriorsGames = [{
     awayTeam: {
-      team: 'Golden State',
-      points: 119,
-      isWinner: true
-    },
-    homeTeam: {
-      team: 'Houston',
-      points: 106,
-      isWinner: false
-    }
-  },
-  {
-    awayTeam: {
-      team: 'Golden State',
-      points: 105,
+      team: 'Cleveland',
+      points: 89,
       isWinner: false
     },
     homeTeam: {
-      team: 'Houston',
-      points: 127,
-      isWinner: true
-    }
-  },
-  {
-    homeTeam: {
       team: 'Golden State',
-      points: 126,
-      isWinner: true
-    },
-    awayTeam: {
-      team: 'Houston',
-      points: 85,
-      isWinner: false
-    }
-  },
-  {
-    homeTeam: {
-      team: 'Golden State',
-      points: 92,
-      isWinner: false
-    },
-    awayTeam: {
-      team: 'Houston',
-      points: 95,
+      points: 104,
       isWinner: true
     }
   },
   {
     awayTeam: {
-      team: 'Golden State',
-      points: 94,
+      team: 'Cleveland',
+      points: 77,
       isWinner: false
     },
     homeTeam: {
-      team: 'Houston',
-      points: 98,
+      team: 'Golden State',
+      points: 110,
       isWinner: true
     }
   },
   {
     homeTeam: {
+      team: 'Cleveland',
+      points: 120,
+      isWinner: true
+    },
+    awayTeam: {
       team: 'Golden State',
+      points: 90,
+      isWinner: false
+    }
+  },
+  {
+    homeTeam: {
+      team: 'Cleveland',
+      points: 97,
+      isWinner: false
+    },
+    awayTeam: {
+      team: 'Golden State',
+      points: 108,
+      isWinner: true
+    }
+  },
+  {
+    awayTeam: {
+      team: 'Cleveland',
+      points: 112,
+      isWinner: true
+    },
+    homeTeam: {
+      team: 'Golden State',
+      points: 97,
+      isWinner: false
+    }
+  },
+  {
+    homeTeam: {
+      team: 'Cleveland',
       points: 115,
       isWinner: true
     },
     awayTeam: {
-      team: 'Houston',
-      points: 86,
+      team: 'Golden State',
+      points: 101,
       isWinner: false
     }
   },
   {
     awayTeam: {
-      team: 'Golden State',
-      points: 101,
+      team: 'Cleveland',
+      points: 93,
       isWinner: true
     },
     homeTeam: {
-      team: 'Houston',
-      points: 92,
+      team: 'Golden State',
+      points: 89,
       isWinner: false
     }
   }
 ]
-
-const ulParent = document.createElement('ul')
-
-for(let game of warriorsGames){
-  const {homeTeam, awayTeam} = game; 
-  const gameLi = document.createElement('li')
-  const {team: hTeam, points: hPoints} = homeTeam;
-  const {team: aTeam, points: aPoints} = awayTeam; 
-  const teamNames = `${aTeam} @ ${hTeam}`
-  let scoreLine; 
-  if(aPoints > hPoints){
-    scoreLine = `<b>${aPoints}</b>-${hPoints}`
-  } else {
-    scoreLine = `${aPoints}-<b>${hPoints}</b>`
+//************************************************* */
+const makeChart = (games, targetTeam) => {
+  const ulParent = document.createElement('ul')
+  for(let game of games){
+    const {homeTeam, awayTeam} = game; 
+    const gameLi = document.createElement('li')
+    gameLi.innerHTML = getScoreLine(game)
+    gameLi.classList.add(isWinner(game, targetTeam) ? 'win' : 'loss')
+  
+    ulParent.appendChild(gameLi)
   }
-  
-  const warriors = hTeam === 'Golden State' ? homeTeam : awayTeam; 
-  gameLi.classList.add(warriors.isWinner ? 'win' : 'loss')
-  
-
-  gameLi.innerHTML = `${teamNames} ${scoreLine}`
-  ulParent.appendChild(gameLi)
+  return ulParent; 
 }
 
-document.body.prepend(ulParent)
+const isWinner = ({homeTeam, awayTeam}, targetTeam) => {
+  const target = homeTeam.team === targetTeam ? homeTeam : awayTeam; 
+  return target.isWinner; 
+}
+
+const getScoreLine = ({homeTeam, awayTeam}) => {
+  const {team: hTeam, points: hPoints} = homeTeam;
+    const {team: aTeam, points: aPoints} = awayTeam; 
+    const teamNames = `${aTeam} @ ${hTeam}`
+    let scoreLine; 
+    if(aPoints > hPoints){
+      scoreLine = `<b>${aPoints}</b>-${hPoints}`
+    } else {
+      scoreLine = `${aPoints}-<b>${hPoints}</b>`
+    }
+    return `${teamNames} ${scoreLine}`
+}
+
+
+const gsSection = document.querySelector('#gs');
+const cavsSection = document.querySelector('#cc');
+
+const gsChart = makeChart(warriorsGames, 'Golden State')
+const cavsChart = makeChart(warriorsGames, 'Cleveland')
+gsSection.appendChild(gsChart)
+cavsSection.appendChild(cavsChart)
+
+
 
